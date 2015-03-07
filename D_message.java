b6f6aa515092e6013c3febd8e31825499747d728
weishi3 <weishi3@illinois.edu>
2015-03-07 10:19:33 -0600

    D_message.java

---
+++ D_message.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class D_message {
+	
+	
+
+		private int deliveryTime;//accept time
+		private int delay;
+		private Pair message;  // the raw message that contains fromNode, toNode, and messageContent
+		private int from;
+		private int to;
+
+		public D_message(int deli, int d, Pair m, int f, int t) {
+			deliveryTime = deli;
+			delay = d;
+			message = m;
+			from = f;
+			to = t;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void try_to_delay() {
+			try {
+				from.getThread_send.sleep(delay);
+			} catch (InterruptedException e) {
+				System.err.println("Sleeping is Interrupted. Controller failure.");
+				System.exit(1);
+			}
+		}
+	
+}
